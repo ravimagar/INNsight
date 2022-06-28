47dcde33334d2fe33a6da7f93ba819a174474b47
ravimagar <102816091+ravimagar@users.noreply.github.com>
2022-06-28 15:21:33 +0530

    Add files via upload
    Interview project

---
+++ innsightbookingproject/src/test/java/testbase/LoginTest.java
@@ -0,0 +1,42 @@
+package testbase;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.BookDirectAndSavePage;
+import pages.HomePage;
+
+public class LoginTest extends TestBase{
+    BookDirectAndSavePage bookDirectAndSavePage;
+    HomePage homePage;
+
+    LoginTest() {
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        homePage = new HomePage(getDriver());
+        bookDirectAndSavePage = new BookDirectAndSavePage(getDriver());
+    }
+
+    @Test
+    public void verifyBookCalender() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        homePage.clickOnReservation();
+        bookDirectAndSavePage.selectCheckInDate("1");
+        bookDirectAndSavePage.selectCheckOutData("2");
+        bookDirectAndSavePage.selectRoomAdultChild(2, 2);
+        bookDirectAndSavePage.searchHotels();
+        softAssert.assertAll();
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        getDriver().quit();
+    }
+}
+
